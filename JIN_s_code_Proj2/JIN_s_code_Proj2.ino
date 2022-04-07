85c26897f14e753802b8053fb69ba3a3a8a6df85
JINZU02 <100441625+JINZU02@users.noreply.github.com>
2022-04-07 14:53:00 -0500

    Created variables and initialized servo and button pins

--- JIN_s_code_Proj2/JIN_s_code_Proj2.ino
+++ JIN_s_code_Proj2/JIN_s_code_Proj2.ino
@@ -1,9 +1,23 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+ AET Chang'e Story
+ Team Name: The Jade Rabbits
+ Team Members: Jose Nerio, Jessica Chen, and Anja Dahnke
+ Description: button and servo program 
+*/
 
+//Servo code library
+#include<Servo.h>;
+
+//VARIABLES
+Servo myservo;
+const int buttonPin = 7;
+
+
+void setup() {
+  myServo.attach(9);
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
 }
