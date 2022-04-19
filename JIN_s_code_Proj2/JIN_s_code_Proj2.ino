3aa9a9084f5629fd86b43f4baad087acdba43767
JINZU02 <100441625+JINZU02@users.noreply.github.com>
2022-04-19 12:04:58 -0500

    Changed functionality and added initialization to servos

--- JIN_s_code_Proj2/JIN_s_code_Proj2.ino
+++ JIN_s_code_Proj2/JIN_s_code_Proj2.ino
@@ -1,8 +1,8 @@
 /*
- AET Chang'e Story
- Team Name: The Jade Rabbits
- Team Members: Jose Nerio, Jessica Chen, and Anja Dahnke
- Description: button and servo program 
+AET Chang'e Story
+Team Name: The Jade Rabbits
+Team Members: Jose Nerio, Jessica Chen, and Anja Dahnke
+Description: button and servo program
 */
 
 //Servo code library
@@ -20,71 +20,62 @@ const int elixirPin = 8;
 const int jadeRabbitPin = 7;
 const int ledPin = 2;
 
-
-
 void setup() {
-  servos();
-  pins();
+ servos();
+ pins();
+ initialize();
 }
-
 
 void loop() {
-  //if button is pressed, swivel servo 180 degrees
-  if(digitalRead(bushPin) == LOW) {
-    houYiServo.write(180);
+ //if button is pressed, swivel servo 180 degrees
+ if(digitalRead(bushPin) == LOW) {
+   houYiServo.write(180);
+   
+   if(digitalRead(elixirPin) == HIGH) {
+     moveChange();
+   } 
+   
+   if(digitalRead(quiverPin) == HIGH) {
+     moveArrow();
+   }
+   
+   if(digitalRead(jadeRabbitPin) == LOW) {  //route 3
+     rabbitMoon();
+   } else {
+    digitalWrite(ledPin, LOW);
+   }
 
-    //part 1
-    if(digitalRead(elixirPin) == HIGH && digitalRead(quiverPin) == LOW) {  //route 1 & 3
-      scene1();
-    } else if(digitalRead(elixirPin == LOW && digitalRead(quiverPin) == HIGH) {  //route 2 (arrow hits chang'e; end of story)
-      scene1.2();
-    }
-
-    //part 2
-    if(digitalRead(quiverPin) == HIGH && digitalRead(elixirPin) == HIGH) {  //route 1
-      scene2();
-    }
-
-    //part 3
-    if(digitalRead(quiverPin) == HIGH && digitalRead(elixirPin) == HIGH && digitalRead(jadeRabbit) == HIGH) {  //route 3
-      scene3(); 
-    }
-  }
-
-
-  
+ }
 }
 
-
-
 void servos() {  //change these numbers if necessary for organization
-  houYiServo.attach(11);  //using .attach to initialize servo to pin 11
-  changeServo.attach(10);
-  arrowServo.attach(9);
+ houYiServo.attach(11);  //using .attach to initialize servo to pin 11
+ changeServo.attach(9);
+ arrowServo.attach(10);
 }
 
 void pins() {
-  pinMode(bushPin, INPUT);
-  pinMode(quiverPin, INPUT);
-  pinMode(elixirPin, INPUT);
-  pinMode(jadeRabbitPin, INPUT);
+ pinMode(bushPin, INPUT);
+ pinMode(quiverPin, INPUT);
+ pinMode(elixirPin, INPUT);
+ pinMode(jadeRabbitPin, INPUT);
+}
+
+void initialize() {
+  houYiServo.write(0);
+ changeServo.write(90);
+ arrowServo.write(120);
+}
+
+void moveChange() {
+ changeServo.write(20);
+}
+
+void moveArrow() {
+ arrowServo.write(40);
 }
 
 
-void scene1() {
-  changeServo.write(90);
-  arrowServo.write(0);
-}
-
-void scene1.2() {
-  arrowServo.write(180);
-  changeServo.write(0);
-}
-
-void scene2() {
-  arrowServo.write(180);
-}
-
-void scene3() {
-  digitalWrite(ledPin, HIGH);
+void rabbitMoon() {
+ digitalWrite(ledPin, HIGH);
 }
